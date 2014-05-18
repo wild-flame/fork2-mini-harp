683e07a7050f98dff60a8ce4cb32b46c3012f3f7
wild-flame <wildflame@126.com>
2014-05-18 22:09:01 +0800

    Finish Jade part

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
-var mini_harp = require("mini-harp"),
+var mini_harp = require("."),
     parseArgs = require("minimist");
 
 var pars = parseArgs(process.argv); // Input parameters
 
-if (pars._[2]) {console.log("ture");} else {console.log("false")};
-mini_harp(pars.port, pars._[2]);
+port = pars.port || 4000;
+root = pars._[2] || process.cwd();
+
+console.log("Starting mini-harp on http://localhost:"+port);
+mini_harp(root).listen(port);
--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-var mini_harp = function(par_port, par_path) {
+var mini_harp = function(root) {
   var app = require("connect")(); //Create connect app
-  var DEFAULT_PORT = 4000; 
-  var DEFAULT_PATH = process.cwd(); // current directory
   var serveStatic = require('serve-static');
+  var makeJade = require('./lib/processor/jade') 
 
-  if (par_port) {
-    port = par_port; // Let the port equals to the specified port
-  } else {
-    port = DEFAULT_PORT; // Let the port be the DEFAULT_PORT 
-  }
-  if (par_path) {
-    path = par_path; 
-  } else {
-    path = DEFAULT_PATH;
-  }
-  console.log("Starting mini-harp on http://localhost:"+port);
-  app.use(serveStatic(path)).use(function(request,response,next) {
-    console.log("request.url: " + request.url)
+  return app.use(makeJade(root))
+     .use(serveStatic(root))
+     .use(function(request,response,next) {
       if (request.url === "/current-time" && request.method === "GET") {
         response.end((new Date()).toISOString()+"\n");
-      } else {
+      }
+      else {
         response.statusCode = 404;
         response.setHeader("Content-Type", "text/html");
         response.end("Cannot GET" + request.url+"\n");
       }
   }
-  ).listen(port); 
+  ); 
 }
 
 module.exports = mini_harp
